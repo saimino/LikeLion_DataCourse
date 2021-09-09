9c52172d55e8bc66d67173ffbf82bb16deabe5ca
saimino <choi@users.noreply.github.com>
2021-09-09 15:23:33 +0900

    fix

--- web_data/12_mulipage.py
+++ web_data/12_mulipage.py
@@ -1,18 +1,19 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import pandas as pd
+import time
 
 ## 1~5페이지 가져오기
 ## 댓글 1페이지의 댓글 전체 가져오기-10개
 url = 'https://movie.naver.com/movie/point/af/list.naver?st=mcode&sword=171725&target=after&page='
 
 full_comment = []
-for i in range(1, 6, 1):
+for i in range(1, 50, 1):
     page_url = url + f"&page={i}"
     page = urlopen(page_url)
     soup = BeautifulSoup(page, "html.parser")
     eval_box = soup.find_all("td", class_="title")
-
+    time.sleep(0.5)
     # comments = []
     for comment in eval_box:
         each_comment = (list(comment.children)[6]).strip()
